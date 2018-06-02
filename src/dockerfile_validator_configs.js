87c07a3ee7d15a8d95aebca83d98ed6a038af733
Miguel Marques <miguel.marques@gohubble.com>
2018-06-02 13:42:51 +0100

    Validate the custom rule files and fallback to default rules if invalid

--- src/dockerfile_validator_configs.js
+++ src/dockerfile_validator_configs.js
@@ -2,6 +2,7 @@
 const vscode = require('vscode');
 const path = require('path');
 const fs = require('fs');
+const DockerFileValidator = require('dockerfile_lint');
 
 class DockerfileValidatorConfigs extends vscode.Disposable { 
     
@@ -16,11 +17,32 @@ class DockerfileValidatorConfigs extends vscode.Disposable {
     static getRuleFilePath(){
         var config = vscode.workspace.getConfiguration('dockerfile-validator');
         var result = config.rulefile.path;
-        if(!fs.existsSync(result)){
-            var errorMessage = `Error loading rule file "${result}": File does not exist.\nUsing default rules instead.`;
+        var validRuleFile =  DockerfileValidatorConfigs.validateRulefile(result, (errorMessage) =>{
+            var errorMessage = `[dockerfile-validator]: Error loading rule file "${errorMessage}":.\nUsing default rules instead.`;
             vscode.window.showWarningMessage(errorMessage);
+        })
+        if(!validRuleFile){
             result = path.join(__dirname, "rule_files", 'default_rules.yaml');
         }
+        return result;
+    }
+
+    static validateRulefile(ruleFilePath, errorCallback){
+        var result = false;
+        
+        if(!fs.existsSync(ruleFilePath)){
+            var errorMessage = `"${ruleFilePath}": File does not exist.`;
+            errorCallback(errorMessage);
+        }
+        else{
+            try {
+                new DockerFileValidator(ruleFilePath);
+                result = true;
+            } catch (error) {
+                errorCallback(error.message)
+            }
+        }
+
         return result;
     }
 }
